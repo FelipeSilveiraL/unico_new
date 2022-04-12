91684823539e6eb6addcd4bae9288c67cd00e4da
FelipeLara <82624468+FelipeSilveiraL@users.noreply.github.com>
2022-04-12 13:00:39 -0300

    Update menu.php

--- front/menu.php
+++ front/menu.php
@@ -1,8 +1,8 @@
 <aside id="sidebar" class="sidebar">
 
     <ul class="sidebar-nav" id="sidebar-nav">
-        
-    <li class="nav-heading">Sistemas</li>
+
+        <li class="nav-heading">Sistemas</li>
 
         <li class="nav-item">
             <a class="nav-link " href="../index.php">
@@ -14,16 +14,23 @@
         <li class="nav-heading">Paginas</li>
 
         <li class="nav-item">
-            <a class="nav-link collapsed" href="users-profile.html">
+            <a class="nav-link collapsed" href="#">
                 <i class="bi bi-person"></i>
                 <span>Meu perfil</span>
             </a>
         </li>
 
         <li class="nav-item" style="display: <?= $_SESSION['admin'] == 1 ?: "none" ?>;">
-            <a class="nav-link collapsed" href="users-profile.html">
+            <a class="nav-link collapsed" href="#">
                 <i class="bi bi-gear"></i>
                 <span>Configurações</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-question-circle"></i>
+                <span>Ajuda</span>
             </a>
         </li>
 
