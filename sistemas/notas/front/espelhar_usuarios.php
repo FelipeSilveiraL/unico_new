e9d6cf8a151a88b33fce979e96c210aa61c4b534
FelipeLara <82624468+FelipeSilveiraL@users.noreply.github.com>
2022-05-23 16:34:53 -0300

    Update espelhar_usuarios.php

--- sistemas/notas/front/espelhar_usuarios.php
+++ sistemas/notas/front/espelhar_usuarios.php
@@ -1,7 +1,7 @@
 <?php
 require_once('head.php'); //CSS e configurações HTML e session start
-require_once('header.php');//logo e login
-require_once('menu.php');//menu lateral da pagina
+require_once('header.php'); //logo e login
+require_once('menu.php'); //menu lateral da pagina
 ?>
 
 <main id="main" class="main">
@@ -16,20 +16,62 @@ require_once('menu.php');//menu lateral da pagina
       </ol>
     </nav>
   </div><!-- End Navegação -->
-  
-  <?php 
-  require_once('../../../inc/mensagens.php');//Alertas
+
+  <?php
+  require_once('../../../inc/mensagens.php'); //Alertas
   require_once('../inc/senhaBPM.php'); //validar se possui senha cadastrada 
   ?>
 
-  <!--################# COLE section AQUI #################-->
+  <section class="section">
+    <div class="row">
+      <div class="col-lg-12">
 
-  <h6>Inicio da section!</h6>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Espelhar usuários</h5>
+            <p>Nesta tela só é permitido fazer espelhamento dentre os usuários. Caso queira mudar outras informações como por exemplo; usuário, senha, etc... Basta clicar neste icone <a href="../../../front/usuarios.php?pg=2&conf=1" target="_blank" class="btn-info btn-sm"><i class="ri-user-settings-line"></i></a></p>
+            <!-- Table with stripped rows -->
+            <table class="table datatable">
+              <thead>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">Usuário</th>
+                  <th scope="col">Espelhador por</th>
+                  <th scope="col">ID Usuarios</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Brandon Jacob</td>
+                  <td>Designer</td>
+                  <td>28</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Bridie Kessler</td>
+                  <td>Developer</td>
+                  <td>35</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Ashleigh Langosh</td>
+                  <td>Finance</td>
+                  <td>45</td>
+                </tr>
+              </tbody>
+            </table>
+            <!-- End Table with stripped rows -->
 
-  <!--################# section TERMINA AQUI #################-->
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
 
 </main><!-- End #main -->
 
 <?php
-  require_once('footer.php');//Javascript e configurações afins
+require_once('footer.php'); //Javascript e configurações afins
 ?>
