ccf470353b6016032f5ec40af2616e86ed7fb8e4
FelipeLara <82624468+FelipeSilveiraL@users.noreply.github.com>
2022-06-08 12:02:21 -0300

    update

--- sistemas/sisrev/config/query.php
+++ sistemas/sisrev/config/query.php
@@ -31,6 +31,19 @@ LIMIT 1;";
 $resultLogPE = $conn->query($queryLogPE);
 $logPE = $resultLogPE->fetch_assoc();
 
-//excluir tabela empresas_bpmgp
-$drop_Empresas = 'DROP TABLE empresas_bpmgp';
 
+$queryTabela = "SELECT * FROM `sisrev_empresas_bpmgp` where ID_EMPRESA NOT IN(302,208,261) ORDER BY id ASC;";
+
+$consorcio = ($row["CONSORCIO"] == 'S') ? 'SIM' : 'NÃO';
+
+$situacao = ($row["SITUACAO"] == 'A') ? 'ATIVO' : 'DESATIVADO';
+
+$valueApollo = ($row["EMPRESA_APOLLO"] == 0) ? '' : $row["EMPRESA_APOLLO"];
+
+$valueRevApollo = ($row["REVENDA_APOLO"] == 0) ? '' : $row["REVENDA_APOLO"];
+
+$valueEmpNbs = ($row["EMPRESA_NBS"] == 0) ? '' : $row["EMPRESA_NBS"];
+
+$editarTabela = "SELECT * FROM `sisrev_empresas_bpmgp` ";
+
+$queryModulos = "SELECT * FROM sisrev_modulos";
---
+++ sistemas/sisrev/front/pesquisa.php
@@ -0,0 +1,53 @@
+<?php
+require_once('head.php'); //CSS e configurações HTML e session start
+require_once('header.php'); //logo e login e banco de dados
+require_once('menu.php'); //menu lateral da pagina
+echo $_POST['pesquisa'] == NULL ? '<script>window.location.href = "index.php";</script>' : '';
+$queryModulos .= " WHERE nome like '%" . $_POST['pesquisa'] . "%'";
+$resultModulos = $conn->query($queryModulos);
+
+echo $queryModulos;
+?>
+
+<main id="main" class="main">
+
+  <div class="pagetitle">
+    <h1>Pesquisando por - <?= $_POST['pesquisa'] ?></h1>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.php?pg=<?= $_GET['pg'] ?>">Home</a></li>
+        <li class="breadcrumb-item">Pesquisa</li>
+      </ol>
+    </nav>
+  </div><!-- End Navegação -->
+
+  <?php
+  require_once('../../../inc/mensagens.php'); //Alertas
+  ?>
+
+  <section class="section">
+    <div class="row">
+      <br>
+      <?php
+
+      while ($modulo = $resultModulos->fetch_assoc()) {
+        echo '<div class="col-lg-3"> 
+                <a href="'.$modulo['endereco'].'" class="list-group-item list-group-item-action">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title capitalize">'.$modulo['nome'].'</h5>
+                    </div>
+                  </div>
+                </a>
+              </div>';
+      }
+      ?>
+    </div>
+
+  </section>
+
+</main><!-- End #main -->
+
+<?php
+require_once('footer.php'); //Javascript e configurações afins
+?>
