19e69be2b7a91795e11feb1a2cdb2c0d87319fb6
FelipeLara <82624468+FelipeSilveiraL@users.noreply.github.com>
2022-06-24 15:45:17 -0300

    Update atualizarPreco.php

--- sistemas/sisrev/inc/atualizarPreco.php
+++ sistemas/sisrev/inc/atualizarPreco.php
@@ -3,6 +3,8 @@ session_start();
 require '../../../vendor/autoload.php'; //autoload da biblioteca
 require_once('../config/query.php');
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet; //classe responsável pela manipulação da planilha
+
 $extXLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
 
 if ($_POST['empresa'] == 1 || $_POST['empresa'] == 2) {
@@ -22,10 +24,40 @@ if ($_POST['empresa'] == 1 || $_POST['empresa'] == 2) {
             //log da carga
             $insertLog = "INSERT INTO sisrev_arquivo_ap (caminho,nome_arquivo,data,id_usuario,id_empresa) VALUES ('" . $uploadfile . "', '" . $_FILES['arquivo']['name'] . "','" . date('Y-m-d H:i:s') . "','" . $_SESSION['id_usuario'] . "','" . $_POST['empresa'] . "')";
             $result = $conn->query($insertLog);
-
             
-
-
+            //limpar banco para receber as informações
+            $truncate = "TRUNCATE `sisrev_atualizacao_preco`";
+            $resultTruncate = $conn->query($truncate);
+            
+            //carrega os arquivos vindo da planilha
+            $arquivo = $uploadfile;
+            $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader("Xlsx");
+            $spreadsheet = $reader->load($arquivo);
+            $sheet = $spreadsheet->getActiveSheet();        
+            
+            foreach ($sheet->getRowIterator(8) as $row) {
+                
+                $cellInterator = $row->getCellIterator();
+                $cellInterator->setIterateOnlyExistingCells(false);
+            
+            
+                $query = "INSERT INTO `sisrev_atualizacao_preco`
+                (`item`,`descricao`,`grupo`,`fiscal`,`rrp`,`total_invoice`,`uf`)
+                VALUES (";
+                
+                //Linha
+                foreach ($cellInterator as $cell) {
+                    if (!is_null($cell)) {
+                        $value = $cell->getCalculatedValue();
+                        if (trim($value) != '') {
+                            $query .= "'".$value."',";
+                        }
+                    }
+                }
+                $count = strlen($query) - 1;
+                $insert = substr($query, 0, $count).")";   
+                $resultado = $conn->query($insert);
+            }
 
         } else {
             header('location: ../front/atualizarPreco.php?pg=' . $_GET['pg'] . '&msn=10&erro=2'); //não foi possivel salvar o arquivo
@@ -34,7 +66,12 @@ if ($_POST['empresa'] == 1 || $_POST['empresa'] == 2) {
         header('location: ../front/atualizarPreco.php?pg=' . $_GET['pg'] . '&msn=10&erro=3'); //extensão do arquivo é invalida
     }
 } else {
+    //HONDA
 }
+
+$conn->close();
+
+echo 'Finalizado';
 
 
 
